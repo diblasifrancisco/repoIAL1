8b8f5b51454658cdfd5877635ce9829cf0c25d56
Francisco Diblasi <diblasifrancisco@outlook.com>
2016-05-08 14:57:22 -0300

    Implementar en el código la alternativa para implementar  Búsqueda Voraz (saco de abierta el mejor en cto a menor función de coste, siendo f(n)= h(n))  (para Aprobar Mínimo) Implementar en el código la alternativa para implementar Búsqueda A* (saco de abierta el mejor en cto a menor función de coste, siendo f(n)= g(n)+h(n)). Justificar como se lleva a cabo el desempate y el método de ordenación utilizado, en caso de ordenar la lista para sacar el menor f(n) (para Alcanzar)

--- L1catedra/src/rubik/busqueda/BusquedaAEstrella.java
+++ L1catedra/src/rubik/busqueda/BusquedaAEstrella.java
@@ -13,9 +13,22 @@ public class BusquedaAEstrella extends BusquedaHeuristica implements Busqueda {
     listaAbierta = new LinkedList<NodoBusqueda>();
     Boolean solucionEncontrada = false;
     NodoBusqueda nodoSolucion = null;
-    /*
-     * .....
-     */
+    NodoBusqueda nodoActual = new NodoBusqueda(inicial,null,null);
+		nodoActual.setProfundidad(0);
+		nodoActual.setCosto(0);
+                /*  asigno heuriristica  */
+		//traza = new TrazaGenerica(nodoActual);
+    listaAbierta.add(nodoActual);
+    while(!solucionEncontrada) {
+      if(listaAbierta.size() == 0) {
+        break;
+      }
+      else {
+	//traza.imprimirInicioIteracion(listaAbierta);
+        /*  estrategia Aestrella en grafo con heurisitica  cambia la forma en que ordena la lista  */
+      }
+    }
+    /*  reportes de rendimiento  */
     if(nodoSolucion == null) {
       return new Vector<Operador>();
     }
@@ -25,4 +38,31 @@ public class BusquedaAEstrella extends BusquedaHeuristica implements Busqueda {
 	}
 
 
-    
+    private void ordenarListaFnEvaluacion() {
+            LinkedList<NodoBusqueda> lista = listaAbierta;
+	    int i, j;
+	    int N = lista.size();
+                 /*ordenacion burbuja segun funcon de evaluacion igual que costo uniforme pero segun f */
+            listaAbierta = lista;
+
+    }
+
+  private NodoBusqueda getNodoMenorFnEvaluacionListaAbierta() {
+    if(listaAbierta.size() == 1) {
+			return listaAbierta.pollFirst();
+		}
+		else {
+			int menorEvaluacion = listaAbierta.element().getCosto() +  listaAbierta.element().getHeuristica();
+			NodoBusqueda menorNodo = listaAbierta.element();
+			for(NodoBusqueda n : listaAbierta) {
+				if((n.getCosto() + n.getHeuristica()) < menorEvaluacion) {
+					menorNodo = n;
+					menorEvaluacion = n.getCosto() + n.getHeuristica();
+				}
+			}
+			listaAbierta.remove(menorNodo);
+			return menorNodo;
+		}
+	}
+
+}
--- L1catedra/src/rubik/busqueda/BusquedaVoraz.java
+++ L1catedra/src/rubik/busqueda/BusquedaVoraz.java
@@ -12,9 +12,22 @@ public class BusquedaVoraz extends BusquedaHeuristica implements Busqueda {
     listaAbierta = new LinkedList<NodoBusqueda>();
     Boolean solucionEncontrada = false;
     NodoBusqueda nodoSolucion = null;
-   /*
-    * .....
-    */
+    NodoBusqueda nodoActual = new NodoBusqueda(inicial,null,null);
+		nodoActual.setProfundidad(0);
+		nodoActual.setCosto(0);
+    nodoActual.setHeuristica(h.obtenerHeuristica(nodoActual));
+		//traza = new TrazaGenerica(nodoActual);
+    listaAbierta.add(nodoActual);
+    while(!solucionEncontrada) {
+      if(listaAbierta.size() == 0) {
+        break;
+      }
+      else {
+	//traza.imprimirInicioIteracion(listaAbierta);
+        /*  estrategia voraz en grafo con heurisitica  cambia la forma en que ordena la lista  */
+      }
+    }
+    /*reportes de rendimiento */
     if(nodoSolucion == null) {
       return new Vector<Operador>();
     }
